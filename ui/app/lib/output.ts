08eadb0cfb0edbc16a1a9463275a126031e2d379
zjp <jiping_zhou@foxmail.com>
2026-01-07 16:29:18 +0000

    ui: declare Tags types

--- ui/app/lib/output.ts
+++ ui/app/lib/output.ts
@@ -8,10 +8,36 @@ export type Function = {
   src: string,
   mir: string,
   doc: string,
+  tags: Tags
+}
+
+export type Tags = {
+  tags: Property[],
+  spec: { [key: string]: TagSpec },
+  docs: string[],
+}
+
+export type Property = {
+  tag: { name: string, typ: TagType | null },
+  args: string,
+}
+
+export type TagSpec = {
+  args: string[],
+  desc: string | null,
+  expr: string | null,
+  types: TagType[],
+  url: string | null,
+}
+
+export enum TagType {
+  Precond = "precond",
+  Hazard = "hazard",
+  Option = "option",
 }
 
 export const EMPTY_FUNCTION: Function = {
   name: "", safe: true, callees: [], adts: {}, span: "",
-  src: "", mir: "", doc: "",
+  src: "", mir: "", doc: "", tags: { tags: [], spec: {}, docs: [] },
 };
 
