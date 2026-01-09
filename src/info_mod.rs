4b1d680fdfcc534f0b43381fbc2b12fe0eaa4b03
zjp <jiping_zhou@foxmail.com>
2026-01-09 12:40:22 +0000

    upg: add module paths for free items

--- src/info_mod.rs
+++ src/info_mod.rs
@@ -1,3 +1,5 @@
+use std::mem;
+
 use crate::FxIndexMap;
 use rustc_hir::{ImplItemImplKind, ImplItemKind, ItemId, ItemKind, OwnerNode, Ty, def_id::DefId};
 use rustc_middle::ty::{TyCtxt, TyKind};
@@ -10,21 +12,97 @@ pub struct Navigation {
     pub navi: Navi,
 }
 
-pub type FlattenFreeItems = Vec<Vec<DefPath>>;
+pub type ItemPath = Vec<DefPath>;
+pub type FlattenFreeItems = Vec<ItemPath>;
 pub type Navi = FxIndexMap<usize, Vec<usize>>;
 
-fn to_navi(v_path: &mut FlattenFreeItems) -> Navi {
-    v_path.sort_unstable();
-    Navi::default()
+fn to_navi(v_path: &mut FlattenFreeItems, tcx: TyCtxt) -> Navi {
+    #[derive(Debug, Default)]
+    struct Meta {
+        parent_paths: FxIndexMap<DefPathKind, ItemPath>,
+        item_idx: usize,
+    }
+    let mut map_paths = FxIndexMap::<ItemPath, Meta>::with_capacity_and_hasher(
+        v_path.len() * 3 / 2,
+        Default::default(),
+    );
+    // Add root module path.
+    let crate_root = DefPath::crate_root(tcx);
+    map_paths.insert(vec![crate_root.clone()], Default::default());
+
+    // Collect mod paths and move all paths to the map.
+    // The idx will be backfilled once v_path is sorted, so 0 is fake here.
+    for free_item in mem::take(v_path) {
+        assert_eq!(
+            free_item[0], crate_root,
+            "{free_item:?} must start from {crate_root:?}"
+        );
+
+        // [..mod_sep, ...]
+        let mod_sep = free_item
+            .iter()
+            .take_while(|p| p.kind == DefPathKind::Mod)
+            .count();
+        let len = free_item.len();
+        assert_ne!(
+            len, mod_sep,
+            "free item {free_item:?} mustn't be a module item"
+        );
+
+        // Insert free item path with parent item path.
+        let mod_path = &free_item[..mod_sep];
+        let item_parent_path = &mut map_paths.entry(free_item.clone()).or_default().parent_paths;
+
+        match free_item[mod_sep].kind {
+            DefPathKind::AssocFn | DefPathKind::Mod => (),
+            // Put the assoc item under an ADT or trait path.
+            kind if mod_sep + 1 != len => {
+                let parent_item_path = &free_item[..mod_sep + 1];
+                item_parent_path.insert(kind, parent_item_path.to_owned());
+            }
+            // Put the ADT or trait under mod path.
+            _ => _ = item_parent_path.insert(DefPathKind::Mod, mod_path.to_owned()),
+        }
+
+        // Insert current and parent module paths.
+        let mut pos = mod_sep;
+        while pos > 1 && !map_paths.contains_key(&mod_path[..pos]) {
+            let current_mod = mod_path[..pos].to_owned();
+            let mod_value = map_paths.entry(current_mod).or_default();
+            let parent = mod_path[..pos - 1].to_owned();
+            mod_value.parent_paths.insert(DefPathKind::Mod, parent);
+            pos -= 1;
+        }
+    }
+
+    // Sort all paths.
+    map_paths.sort_unstable_keys();
+    // Set idx by the order.
+    for (idx, value) in map_paths.values_mut().enumerate() {
+        value.item_idx = idx;
+    }
+
+    let mut navi = Navi::default();
+    for current_meta in map_paths.values() {
+        for parent_path in current_meta.parent_paths.values() {
+            let parent_meta = map_paths.get(parent_path).unwrap();
+            navi.entry(parent_meta.item_idx)
+                .and_modify(|v| v.push(current_meta.item_idx))
+                .or_insert_with(|| vec![current_meta.item_idx]);
+        }
+    }
+
+    // Flatten all paths.
+    v_path.extend(map_paths.into_keys());
+
+    navi
 }
 
 pub fn mod_tree(tcx: TyCtxt) -> Navigation {
     let mut v_path = FlattenFreeItems::new();
 
-    // Root module.
-    v_path.push(vec![DefPath::crate_root(tcx)]);
-
-    // Free items.
+    // Free items: those items may be inaccesible from user's perspective,
+    // and item paths are as per source code definitions.
     for item_id in tcx.hir_free_items() {
         let item = tcx.hir_item(item_id);
         match &item.kind {
@@ -59,7 +137,7 @@ pub fn mod_tree(tcx: TyCtxt) -> Navigation {
                                 if let Ok(did) = trait_item_def_id {
                                     let mut trait_name = def_path(did, tcx);
                                     // Put SelfTy under trait path.
-                                    trait_name.extend(std::mem::take(&mut implementor_path));
+                                    trait_name.extend(mem::take(&mut implementor_path));
                                     implementor_path = trait_name;
                                     implementor_path
                                         .push(DefPath::new(DefPathKind::AssocFn, fn_name));
@@ -74,7 +152,7 @@ pub fn mod_tree(tcx: TyCtxt) -> Navigation {
         }
     }
 
-    let navi = to_navi(&mut v_path);
+    let navi = to_navi(&mut v_path, tcx);
     Navigation {
         flatten: v_path,
         navi,
@@ -108,7 +186,7 @@ fn push_parent_paths(path: &mut Vec<DefPath>, item_id: &ItemId, tcx: TyCtxt) {
     path.reverse();
 }
 
-#[derive(Debug, Serialize, PartialEq, PartialOrd, Eq, Ord)]
+#[derive(Clone, Debug, Serialize, PartialEq, PartialOrd, Eq, Ord, Hash)]
 pub struct DefPath {
     pub kind: DefPathKind,
     pub name: Box<str>,
@@ -148,7 +226,7 @@ impl DefPath {
 /// * `[SelfTy, AssocFn]` for an unusual associated function like `impl &Adt`.
 /// * `[Mod, ImplTrait, SelfTy, AssocFn]` for an unusual trait function like `impl Trait for &Adt`,
 ///   `impl Trait for (Adt1, Adt2)`, `impl<T> Trait for T`, or even `impl<T: Trait> Trait for T::U`.
-#[derive(Debug, Serialize, PartialEq, PartialOrd, Eq, Ord)]
+#[derive(Clone, Copy, Debug, Serialize, PartialEq, PartialOrd, Eq, Ord, Hash)]
 pub enum DefPathKind {
     Mod,
     Fn,
